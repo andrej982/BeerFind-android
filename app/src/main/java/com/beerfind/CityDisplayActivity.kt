97a1023dc95469d840f4a2d06dce88755c408e62
gafrika <gafrik.andrej@kingparking.sk>
2022-08-14 18:46:42 +0200

    added some fonts

--- app/src/main/java/com/beerfind/CityDisplayActivity.kt
+++ app/src/main/java/com/beerfind/CityDisplayActivity.kt
@@ -2,7 +2,6 @@ package com.beerfind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class CityDisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
